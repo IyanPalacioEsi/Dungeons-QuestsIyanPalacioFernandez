49904a88b0d823817a85a033af462f62c84f7152
IyanPalacioEsi <ipalaciof@esimurcia.com>
2024-02-26 11:58:48 +0100

    Cambios de KillPlayer y creación de escenas
    Duplicación de escenas y implementación del KillPlayer.

--- Assets/Code/Scripts/Player/KillPlayer.cs
+++ Assets/Code/Scripts/Player/KillPlayer.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class KillPlayer : MonoBehaviour
 {
-    public PlayerHealthController _pHReference;
+    //Referencia al PlayerHealthController
+    private PlayerHealthController _pHReference;
+    //Referencia a UIController
+    private UIController _uIReference;
+    //Referencia al LevelManager
+    private LevelManager _lMReference;
 
     private void Start()
     {
+        //Inicializamos la referencia al PlayerHealthController
         _pHReference = GameObject.Find("Player").GetComponent<PlayerHealthController>();
+        //Inicializamos la referencia al UIController
+        _uIReference = GameObject.Find("Canvas").GetComponent<UIController>();
+        //Inicializamos la referencia al UIController
+        _lMReference = GameObject.Find("LevelManager").GetComponent<LevelManager>();
     }
-    //Método que detecta cuando el jugador se mete dentro de un trigger
+
+    //Método para saber cuando el jugador ha entrado en la zona de muerte
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Comprobamos si es el jugador el que ha entrado en la zona de trigger
+        //Si el jugador es el que ha entrado en la zona
         if (collision.CompareTag("Player"))
         {
-            
+            //Ponemos las vidas del jugador a 0
+            _pHReference.currentHealth = 0;
+            //Actualizamos las vidas en la UI
+            _uIReference.UpdateHealthDisplay();
+            //Respawneamos al jugador
+            _lMReference.RespawnPlayer();
         }
     }
 
