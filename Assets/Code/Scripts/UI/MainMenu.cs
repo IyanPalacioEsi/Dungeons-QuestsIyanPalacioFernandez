52dc870ba2592ad0be259f150203bc38f56af514
IyanPalacioEsi <ipalaciof@esimurcia.com>
2024-03-08 13:13:09 +0100

    Implementación de Main Menu y LS
    Implementación casi completa de Main Menu y LS

--- Assets/Code/Scripts/UI/MainMenu.cs
+++ Assets/Code/Scripts/UI/MainMenu.cs
@@ -12,6 +12,20 @@ public class MainMenu : MonoBehaviour
     {
         
     }
+    //Método para el Botón Start
+    public void StartGame()
+    {
+        //Para saltar a la escena que le pasamos en la variable
+        SceneManager.LoadScene(startScene);
+    }
 
-   
+    //Método para el Botón Quit
+    public void QuitGame()
+    {
+        //Para quitar el juego (solo funciona en la Build no en el editor)
+        Application.Quit();
+        //Feedback para el editor
+        Debug.Log("Quitting Game");
+    }
+
 }
