a0be5583f2d88d2e4097046c09970493243d31f0
IyanPalacioEsi <ipalaciof@esimurcia.com>
2024-03-12 11:55:35 +0100

    Cambios 12 de Marzo
    Cambios realizados el 12 de Marzo.

--- Assets/Code/Scripts/LevelSelector/MapPoint.cs
+++ Assets/Code/Scripts/LevelSelector/MapPoint.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class MapPoint : MonoBehaviour
 {
+    //Declaramos referencias a los MapPoints adyacentes
+    public MapPoint up, right, down, left;
+    //Variable para conocer si este MapPoint es un nivel
+    public bool isLevel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
